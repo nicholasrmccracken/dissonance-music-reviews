ac785a767ccc4eb4404d728cddf6a9164606aade
Austin Bian <iaustin0b@gmail.com>
2025-02-21 02:19:56 -0500

    update

--- app/src/main/java/com/dissonance/app/screens/ProfileEditScreen.kt
+++ app/src/main/java/com/dissonance/app/screens/ProfileEditScreen.kt
@@ -1,7 +1,9 @@
 package com.dissonance.app.screens
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.dissonance.app.R
 
 class ProfileEditScreen : AppCompatActivity() {
@@ -9,5 +11,12 @@ class ProfileEditScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         setContentView(R.layout.activity_profile_editor)
+
+        val backButton = findViewById<Button>(R.id.backButton)
+
+        backButton.setOnClickListener {
+            val intent = Intent(this, ProfileScreen::class.java)
+            startActivity(intent)
+        }
     }
 }
