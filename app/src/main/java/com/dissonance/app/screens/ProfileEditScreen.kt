406d0f6d7179d10760ec49f912c3b4cf3435c8e3
Austin Bian <iaustin0b@gmail.com>
2025-02-21 14:05:41 -0500

    Log cat messages

--- app/src/main/java/com/dissonance/app/screens/ProfileEditScreen.kt
+++ app/src/main/java/com/dissonance/app/screens/ProfileEditScreen.kt
@@ -3,11 +3,14 @@ package com.dissonance.app.screens
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import com.dissonance.app.R
 
 class ProfileEditScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("Profile screen editor onCreate log", "ProfileScreenEditor Activity onCreate() Called")
+
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         setContentView(R.layout.activity_profile_editor)
@@ -18,5 +21,10 @@ class ProfileEditScreen : AppCompatActivity() {
             val intent = Intent(this, ProfileScreen::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Profile screen editor onDestory log", "ProfileScreenEditor Activity onDestroy() Called")
     }
 }
--- app/src/main/java/com/dissonance/app/screens/ProfileScreen.kt
+++ app/src/main/java/com/dissonance/app/screens/ProfileScreen.kt
@@ -3,6 +3,7 @@ package com.dissonance.app.screens
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +12,7 @@ import com.dissonance.app.fragments.EditProfileFragment
 
 class ProfileScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("Profile screen onCreate log", "ProfileScreen Activity onCreate() Called")
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         setContentView(R.layout.activity_profile) // Link to XML layout
@@ -21,5 +23,10 @@ class ProfileScreen : AppCompatActivity() {
             val intent = Intent(this, ProfileEditScreen::class.java)
             startActivity(intent) // Navigate to ProfileScreen
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("Profile screen onDestory log", "ProfileScreen Activity onDestroy() Called")
     }
 }
