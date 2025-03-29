c69323f78b35636a9e62f968ae9339dfc3be878c
Nicholas McCracken <nicholas.r.mccracken@gmail.com>
2025-03-29 19:42:43 -0400

    Implement basic review creation functionality with Firestore

---
+++ app/src/main/java/com/dissonance/app/data/model/Review.kt
@@ -0,0 +1,10 @@
+package com.dissonance.app.data.model
+
+data class Review(
+    val userId: String = "",
+    val albumId: String = "",
+    val rating: Int = 0,
+    val reviewTitle: String = "",
+    val reviewText: String = "",
+    val timestamp: Long = System.currentTimeMillis()
+)
