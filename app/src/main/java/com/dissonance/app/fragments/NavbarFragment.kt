dda6b974247b50784964c1ba22211c1959cd18a8
Nicholas McCracken <nicholas.r.mccracken@gmail.com>
2025-03-29 21:04:33 -0400

    Implement placeholder home screen

--- app/src/main/java/com/dissonance/app/fragments/NavbarFragment.kt
+++ app/src/main/java/com/dissonance/app/fragments/NavbarFragment.kt
@@ -13,6 +13,7 @@ import com.dissonance.app.R
 import com.dissonance.app.screens.ProfileScreen
 import com.dissonance.app.screens.CreateReviewScreen
 import com.dissonance.app.screens.SearchScreen
+import com.dissonance.app.screens.HomeScreen
 
 class NavbarFragment : Fragment() {
 
@@ -35,13 +36,13 @@ class NavbarFragment : Fragment() {
             val currentActivity = requireActivity()::class.java.simpleName
 
             when (item.itemId) {
-//                R.id.nav_home -> {
-//                    if (currentActivity != "HomeScreen") {
-//                    val intent = Intent(requireActivity(), HomeScreen::class.java)
-//                    startActivity(intent)
-//                    }
-//                    true
-//                }
+                R.id.nav_home -> {
+                    if (currentActivity != "HomeScreen") {
+                    val intent = Intent(requireActivity(), HomeScreen::class.java)
+                    startActivity(intent)
+                    }
+                    true
+                }
                 R.id.nav_search -> {
                     if (currentActivity != "SearchScreen") {
                         val intent = Intent(requireActivity(), SearchScreen::class.java)
@@ -83,12 +84,12 @@ class NavbarFragment : Fragment() {
     private fun getCurrentMenuItemId(): Int {
         val currentActivity = requireActivity()::class.java.simpleName
         return when (currentActivity) {
-//            "HomeScreen" -> R.id.nav_home
+            "HomeScreen" -> R.id.nav_home
             "SearchScreen" -> R.id.nav_search
             "CreateReviewScreen" -> R.id.nav_create_review
 //            "ChartsScreen" -> R.id.nav_charts
             "ProfileScreen" -> R.id.nav_profile
-            else -> R.id.nav_profile // TODO: Update to nav_home when home screen is complete
+            else -> R.id.nav_home
         }
     }
 }
